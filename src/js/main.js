d2489edcc92c8eceaed72c0690350b63ccddf832
Dmitry <zimokk1@gmail.com>
2016-07-19 12:38:35 +0200

    Created separated non-conflict jQery fun & moved there buttonsCreation

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,23 @@
+(function( $ ){
+    $.fn.initMap = function() {
+        this.each(function(){
+            var mapContent = new MapContent(this);
+            mapContent.initMap();
+            var $nav = $("<nav></nav>");
+            var buttonAdd = $("<button class='add'>Add polygon</button>").click(function() {
+                mapContent.toggleMarkerListener();
+            });
+            var buttonDelete = $("<button class='delete'>Delete polygon</button>").click(function() {
+                mapContent.deletePolygon();
+            });
+            var buttonDeleteAll = $("<button class='deleteAll'>Delete all</button>").click(function() {
+                mapContent.deleteAllPolygons();
+            });
+            $(this).append($nav.append(buttonAdd,buttonDelete,buttonDeleteAll));
+        });
+        return this;
+    };
+})( jQuery );
 $(document).ready(function(){
-    var mapContent = new MapContent();
-    mapContent.initMap();
-    $("button#add").click(function(){
-        mapContent.toggleMarkerListener();
-    });
-    $("button#delete").click(function(){
-        mapContent.deletePolygon();
-    });
-    $("button#deleteAll").click(function(){
-        mapContent.deleteAllPolygons();
-    })
+    $('div.map').initMap();
 });
